c56c4729f5fd24bcc47cdf1a37d57178b337b828
prehensile <me@prehensile.co.uk>
2016-08-12 21:16:09 +0100

    added finer-grained state tracking for nic and some debug output

--- lib/wifi.py
+++ lib/wifi.py
@@ -7,8 +7,17 @@ import os
 import json
 import pyb
 
+
+class NICState:
+    NOT_CONNECTED = 1
+    NO_VISIBLE_SSIDS = 2
+    CONNECTING = 3
+    CONNECTED = 3
+
 _nic = None
 _network = None
+_state = NICState.NOT_CONNECTED
+
 
 def nic():
     global _nic
@@ -53,13 +62,27 @@ def rssi():
     return v
         
 def connect(wait = True, timeout = 10):
-    global _network
     
-    if nic().is_connected():
+    print( "wifi.connect(wait=%r)" % (wait) )
+    
+    global _network, _state
+
+    if is_connected():
+        print( "-> already connected.")
         return
+    _state = NICState.CONNECTING
     
     networks = connection_details()
     visible_ssids = [ ap['ssid'] for ap in nic().list_aps() ]
+    print( "-> visible_ssids: %r" % visible_ssids )
+
+    known_ssids = [ ap['ssid'] for ap in networks ]
+    print( "-> known_ssids: %r" % known_ssids )
+
+    if len(set(known_ssids).intersection(visible_ssids)) < 1:
+        print( "--> no known ssids visible!" )
+        _state = NICState.NO_VISIBLE_SSIDS
+        return
 
     exc = None
     for details in networks:
@@ -82,6 +105,7 @@ def connect(wait = True, timeout = 10):
                     else:
                         nic().connect(details["ssid"], timeout=None)
             except OSError as e:
+                # hold OSErrors and if we fail to connect to anything, throw the most recent
                 exc = e
 
             # found an AP to connect to, don't bother trying any more
@@ -91,12 +115,35 @@ def connect(wait = True, timeout = 10):
         raise exc
 
 def wait_for_connection():
+    print( "wifi.py: wait_for_connection()" )
+    global _state
     while not nic().is_connected():
         nic().update()
         pyb.delay(100)
+    _state = NICState.CONNECTED
 
 def is_connected():
-    return nic().is_connected()
+    global _state
+    c = nic().is_connected()
+    if c: 
+        _state = NICState.CONNECTED
+    return c
 
 def connection_text():
-    return "Connecting to wifi. If this doesn't work, please check your wifi.json. More information: badge.emfcamp.org/TiLDA_MK3/wifi" 
+    global _state
+    text = ""
+    
+    if (_state == NICState.NOT_CONNECTED) or (_state == NICState.CONNECTING):
+        text = "Connecting to wifi"
+        if _network is not None:
+            text += " %s" % (ssid()) 
+        text += ". If this doesn't work, please check your wifi.json."
+
+    elif _state == NICState.NO_VISIBLE_SSIDS:
+        text = "No visible wifi networks have details in your wifi.json."
+
+    elif _state == NICState.CONNECTED:
+        text = "Wifi connnected to %." % (ssid())        
+    
+    text += " More information: badge.emfcamp.org/TiLDA_MK3/wifi"
+    return text
